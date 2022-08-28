acb9d7df233dc622b037e6163ad056f46d552982
Gich-a <89920190+Gich-a@users.noreply.github.com>
2022-08-28 06:53:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents Healthy New Diner')
+streamlit.title('My Mom's Healthy New Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
